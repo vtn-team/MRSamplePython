b80fd327fd08cd30bf2d0293ecc8257c51ccd65d
k.mitarai <65850380+nyangame@users.noreply.github.com>
2023-06-14 10:18:50 +0900

    Update detect.py

--- detect.py
+++ detect.py
@@ -2,12 +2,15 @@ import numpy as np
 import cv2
 from cv2 import aruco
 
+#設定変数
+CAM_INDEX = 3 #番号(3)は各自の環境で変わるので適宜変える事、何番なのかは力業で調べるしかないっぽい
+
 
 #ArUcoボードの生成と表示(これは画像を保存してUnity側でその画像を表示する)
-cellSize = 35
+cellSize = 35 #ここは適当
 boardDict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
 parameters = aruco.DetectorParameters()
-board = aruco.CharucoBoard((9, 5), 16*cellSize, 9*cellSize, boardDict)
+board = aruco.CharucoBoard((4, 4), 16*cellSize, 9*cellSize, boardDict)
 imboard = board.generateImage((1920, 1080))
 
 #デバッグ表示
@@ -18,10 +21,10 @@ cv2.imshow('ArUcoBoard' , imboard)
 mdetector = aruco.ArucoDetector(boardDict)  #マーカー検知器
 detector = aruco.CharucoDetector(board)     #ボード検知器
 
-cap = cv2.VideoCapture(3) #番号は各自の環境で変わる
-cap.set(cv2.CAP_PROP_FPS, 30) 
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap = cv2.VideoCapture(CAM_INDEX) 
+cap.set(cv2.CAP_PROP_FPS, 30) #60出せるなら60がいい…
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280) #検知が弱かったら上げる事。解像度が対応しているかは調べる事。
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720) #検知が弱かったら上げる事。解像度が対応しているかは調べる事。
 
 #ArUcoボードの検知とキャリブレーション
 while True:
@@ -38,7 +41,10 @@ while True:
 	
 	#画像補正
 	mat = cv2.cvtColor(frame, cv2.COLOR_RGBA2RGB)
+	
+	#検知が弱いかカメラを逆にするときはflipをかける(逆さだと検知されない)
 	#mat = cv2.flip(frame, -1)
+	
 	
 	#こっちでもできる
 	#corners, ids, rejectedImgPoints = mdetector.detectMarkers(mat)
@@ -50,7 +56,9 @@ while True:
 	cv2.imshow('camera' , mat)
 	
 	#Idが0,1,4,5のマーカーを探して、画面の位置を特定する
-	p0 = p1 = p8 = p9 = None
+	#ct, lt, rt, lb, rb
+	mkpos = [[],[],[],[],[]]
+	check = []
 	
 	#要コンバート
 	try:
@@ -58,50 +66,35 @@ while True:
 			for i in range(ids.shape[0]):
 				id = ids[i,0]
 				
+				if id == 4:
+					check = np.append(check, 0)
+					mkpos[0] = (corners[i,0,0],corners[i,0,1])
 				if id == 0:
-					p0 = (corners[i,0,0],corners[i,0,1])
-				if id == 1:
-					p1 = (corners[i,0,0],corners[i,0,1])
+					check = np.append(check, 1)
+					mkpos[1] = (corners[i,0,0],corners[i,0,1])
+				if id == 2:
+					check = np.append(check, 2)
+					mkpos[2] = (corners[i,0,0],corners[i,0,1])
+				if id == 6:
+					check = np.append(check, 3)
+					mkpos[3] = (corners[i,0,0],corners[i,0,1])
 				if id == 8:
-					p8 = (corners[i,0,0],corners[i,0,1])
-				if id == 9:
-					p9 = (corners[i,0,0],corners[i,0,1])
-
-				#公式のキャリブレーション
-#				cameraMatrixInit = np.array([
-#					[ 1000.,    0., imsize[1]/2.],
-#					[    0., 1000., imsize[0]/2.],
-#					[    0.,    0.,           1.]])
-#				distCoeffsInit = np.zeros((5,1))
-#				retval, cameraMatrix, distCoeffs, rvecs, tvecs = aruco.calibrateCameraCharuco(
-#					listCharucoCorners,
-#					listCharucoIds,
-#					board,
-#					(imsize[1],imsize[0]),
-#					cameraMatrixInit,
-#					distCoeffsInit
-#				)
-#				print(retval)
+					check = np.append(check, 4)
+					mkpos[4] = (corners[i,0,0],corners[i,0,1])
+				
 	except ValueError as e:
 		print(e)
 	
-	if p0 == None or p1 == None or p8 == None or p9 == None:
+	if len(check) != 5:
 		continue
 
 	#台形補正
-	sub = ((p1[0] - p0[0]), (p8[1] - p0[1]));
-	pos1 = ((p0[0] - sub[0]), (p0[1] - sub[1]))
-	pos2 = ((p1[0] + sub[0] * 7), (p8[1] + sub[1]*3))
-
-	print(p0)
-	print(p1)
-	print(p8)
-	print(p9)
-	print(sub)
-	print(pos1)
-	print(pos2)
-
-	srcPts = np.array([pos1, [pos2[0], pos1[1]], [pos1[0], pos2[1]], pos2], dtype=np.float32)
+	#四角系で回転を考慮したキャリブレーション
+	edgepos = [[0,0],[0,0],[0,0],[0,0]]
+	for i in range(1,5):
+		edgepos[i-1] = (mkpos[i][0] + mkpos[i][0] - mkpos[0][0], mkpos[i][1] + mkpos[i][1] - mkpos[0][1])
+	
+	srcPts = np.array(edgepos, dtype=np.float32)
 	dstPts = np.array([[0, 0], [imsize[1], 0], [0, imsize[0]], [imsize[1], imsize[0]]], dtype=np.float32)
 	
 	rMat = cv2.getPerspectiveTransform(srcPts, dstPts);
